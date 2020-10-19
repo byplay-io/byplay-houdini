317924609c86da009ec43c4344509eb5cc136692
Vadim Lobanov <vadim@lobanov.pw>
2020-10-19 13:32:46 +0300

    add backend

---
+++ byplay/backend/async_post.py
@@ -0,0 +1,18 @@
+import logging
+
+import requests
+from queue import Queue
+import threading
+
+
+def _sync_post(url, params):
+    logging.info("Doing async request to {}".format(url))
+    res = requests.post(url, **params)
+    logging.info("Async response: {}".format(res))
+
+
+def async_post(url, **params):
+    logging.info("Starting async request to {}".format(url))
+    thread = threading.Thread(target=_sync_post, args=(url, params))
+    thread.setDaemon(True)
+    thread.start()
